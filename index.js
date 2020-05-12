34e286d2ad4ded99c027370482a69dbb22e8164a
Hardik <hardikgupta80@gmail.com>
2020-05-12 16:36:58 +0530

    There was a mistake in previous index.js File. This one is working

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 8000;
 
 // importing the router which is exported by routes //
-const router = require("./routes/index");
+const router = require("./routes");
 
 // Middleware To use the above imported router from ////routes/index.js
 app.use("/", router);
 
-app.listen(function (err) {
+app.listen(port, function (err) {
 	if (err) {
 		console.log(`Error in Running The Server : ${err}`);
 	} else {
