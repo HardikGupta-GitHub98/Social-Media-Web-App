29f92becc017cd0458612554c9906e00590f3940
Hardik <hardikgupta80@gmail.com>
2020-05-12 21:03:01 +0530

    Added The assets Folder For Static Files

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// Importing express-ejs-layouts
+const expressLayouts = require("express-ejs-layouts");
+// Middleware to use Layouts;
+// It sholud be Imoprted Before Importing Routers
+app.use(expressLayouts);
+
+// Settting up the Assets Folder For Static Files
+const assets = express.static("./assets");
+app.use(assets);
+
 // importing the router which is exported by routes //
 const router = require("./routes/index");
 
