6c759ea90c897d4fe4260bfbddf175a885dcab05
Hardik <hardikgupta80@gmail.com>
2020-05-12 15:23:22 +0530

    Commit after changing name in package.json

--- Controllers/home_Controller.js
+++ Controllers/home_Controller.js
@@ -1,5 +1,3 @@
-console.log("Home COntroller Called");
-
 module.exports.home = function (req, res) {
-	return res.send("<h1> Home Controller Exporting! </h1>");
+	return res.end("<h1> Home Controller Exporting! </h1>");
 };
