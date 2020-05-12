12d25dfeeea1b7199cb3f55306e9eb9839fd8364
Hardik <hardikgupta80@gmail.com>
2020-05-12 17:57:50 +0530

    EJS is added and the viewengine along with the path is Set Using app.set

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const router = require("./routes/index");
 // Middleware To use the above imported router from ////routes/index.js
 app.use("/", router);
 
+// Setting Up the View Engine to  render the HTML page as response to a request
+app.set("view engine", "ejs");
+// to set the path for getting the HTML files from
+app.set("views", "./views");
+
 app.listen(port, function (err) {
 	if (err) {
 		console.log(`Error in Running The Server : ${err}`);
